95b803cedfe229701bc0d045eb47d132bd060146
Karthik <134086496+karthik-joseph@users.noreply.github.com>
2025-03-17 12:36:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2025 [Your Name]
+# Copyright (c) 2025 Karthik P
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 from datetime import datetime
